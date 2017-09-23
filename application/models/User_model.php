472222140afd6bdee3f248c8fe59d0ec4e5e01ae
christiantuvieron <30515635+christiantuvieron@users.noreply.github.com>
2017-09-22 20:43:29 -0700

    Add files via upload

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,5 +1,71 @@
 <?php
 	class User_model extends CI_Model{
+		public function __construct(){
+			parent::__construct();
+		}
+		// C
+		public function signup($fname,$lname,$address,$contact,$email,$password){
+			$client_id = "0000";
+			$query = $this->db->query("SELECT client_id FROM client order by client_id ASC");
+			foreach($query->result_array() as $row){
+				$client_id = $row["client_id"];				
+			}
+			$temp = (int)$client_id + 1;
+			$client_id = "0000".$temp;
+			$client_id = substr($client_id,strlen($client_id)-4,strlen($client_id));			
+			$qry = "INSERT INTO client VALUES ('$client_id', '$fname', '$lname', '$email', '$address', '$contact', '$password')";
+			if($this->db->simple_query($qry)){
+				echo "wow";
+			}
+			else{				
+				// echo "'".$client_id."','".$fname.",'".$lname."','".$email."','".$address."','".$contact."','".$password."'";
+				echo $qry;
+			}
+		}
+
+		// R
+		public function login($email,$password){
+
+			//
+			$query = $this->db->query("SELECT FROM client WHERE email = $email AND password = $password");
+			
+			// results from querry will be looped
+
+			if(mysqli_num_rows($query==1)){
+				if(isset($_REQUEST['email'] == '$email' && $_REQUEST['password'] == '$password')){
+				
+					foreach($query->result_array() as $row){
+						$client_id = $row["client_id"];
+						$fname = $row["fname"];
+						$lname = $row["lname"];
+						$email = $row["email"];
+						$address = $row["address"];
+						$contact = $row["contact"];					
+						echo "Welcome";
+					}
+				}
+
+				else{
+					echo "Record not found!";
+				}
+			}
+			
+			else{
+				echo "Error";
+			}
+
+			
+
+
+
+		$data = fetchAll();
+		return ($data);  
+		
+
+		}
+
+		
+
 
 	}
 ?>
