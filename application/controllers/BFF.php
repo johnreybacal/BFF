297b061434f71c6ce0be57b7806ee3df635b883c
christiantuvieron <30515635+christiantuvieron@users.noreply.github.com>
2017-09-24 03:29:56 -0700

    Add files via upload

--- application/controllers/BFF.php
+++ application/controllers/BFF.php
@@ -6,138 +6,116 @@ class BFF extends CI_Controller {
 	public function __construct(){
 		parent::__construct();		
 		$this->load->model('User_model', 'user');
-		$this->load->model('BFF_model', 'bff');
 		$this->load->helper('url');
-		$this->load->library('cart');
 	}
 
 	public function index(){
+		if($this->session->userdata('client_id'))
 		redirect(base_url('BFF/Home/'));
 	}
 
 	public function Home(){	
-		$this->load->view('includes/header.php');
+		if($this->session->userdata('client_id'))
 		$this->load->view('index');
-		$this->load->view('includes/footer.php');
 	}
 
 	public function Album(){
-		$this->load->view('includes/header.php');
+		if($this->session->userdata('client_id'))
 		$this->load->view('album');
-		$this->load->view('includes/footer.php');
 	}
 
 	public function Order(){
-		$data['PnS'] = $this->bff->dummyData();
-		$this->load->view('includes/header.php');
-		$this->load->view('pao', $data);
-		$this->load->view('includes/footer.php');
+		if($this->session->userdata('client_id'))
+		$this->load->view('pao');
 	}
 
 	public function Products_and_Services($pns = null){
-		$this->load->view('includes/header.php');
 		switch($pns){
-			case "Weddings":	$this->load->view('PnS/wedding');	break;
-			case "Inaugurals":	$this->load->view('PnS/inaugural');	break;			
-			case "Bouquet":		$this->load->view('PnS/bouquet');	break;			
-			case "Vase":		$this->load->view('PnS/vase');	break;			
-			case "Sympathy":	$this->load->view('PnS/sympathy');	break;
-			case "Fruits_with_Balloons_and_Decor":	$this->load->view('PnS/fruit');	break;
-			default:	$this->load->view('PnS/pns');	break;
+			case "Weddings":	$this->load->view('wedding');	break;
+			case "Inaugurals":	$this->load->view('inaugural');	break;			
+			case "Bouquet":		$this->load->view('bouquet');	break;			
+			case "Vase":		$this->load->view('vase');	break;			
+			case "Sympathy":	$this->load->view('sympathy');	break;
+			case "Fruits_with_Balloons_and_Decor":	$this->load->view('fruit');	break;
+			default:	$this->load->view('pns');	break;
 		}		
-		$this->load->view('includes/footer.php');
 	}
 
 	public function AboutUs(){
-		$this->load->view('includes/header.php');
+		if($this->session->userdata('client_id'))
 		$this->load->view('aboutus');		
-		$this->load->view('includes/footer.php');
 	}
 
 	public function ContactUs(){
-		$this->load->view('includes/header.php');
+		if($this->session->userdata('client_id'))
 		$this->load->view('contactus');
-		$this->load->view('includes/footer.php');
 	}
 
 	public function Signup(){
 		// tovs uncomment mo lng to
 		// C for Create
 		// function sa model: login($fname, $lname, $addr, $contact, $email, $password)
-		// if($this->input->post('submit')){
-		// 	$this->user->signup($this->input->post('fname'), $this->input->post('lname'), $this->input->post('addr'), $this->input->post('contact'), $this->input->post('email'), $this->input->post('password'));
-		// }
-		$this->load->view('includes/headerWithoutNav.php');
-		$this->load->view('signup');
-		$this->load->view('includes/footer.php');
+		if($this->input->post('submit')){
+			$this->user->signup($this->input->post('fname'), $this->input->post('lname'), $this->input->post('addr'), $this->input->post('contact'), $this->input->post('email'), $this->input->post('password'));
+		}
+		else{
+			$this->load->view('signup');
+		}
 	}
 
 	public function Login(){
 		// tovs uncomment mo lng to
 		// R for Read
 		// function sa model: login($email, $password)
-		// if($this->input->post('submit')){
-		// 	$this->user->login($this->input->post('email'), $this->input->post('password'));
-		// }
-		$this->load->view('includes/headerWithoutNav.php');
-		$this->load->view('login');
-		$this->load->view('includes/footer.php');
+		if($this->input->post('submit')){
+			$this->user->login($this->input->post('email'), $this->input->post('password'));
+		
+				$this->load->view('login');	
+
+				if($this->User_model->check($email,$password)){
+					$result =$this->User_model->get_data($email,$password);
+						foreach($result as $obj){
+						$session_data = array(
+							'client_id'=>$client_id,
+							'email' =>$obj->email,
+							'firstname' =>$obj->fname,
+							'lastname' =>$obj->lname,
+							'address' =>$obj->address,
+							'contact' =>$obj->contact,
+						);
+						}
+						$this->session->set_userdata($session_data);
+						redirect(base_url().'home');
+					}
+				else{
+					
+					$data['flash_error']=$this->session->set_flashdata('error',"Username or password doesn't match!");
+						
+						$this->load->view('login',$data);
+						redirect(base_url().'login');
+						
+					}
+			
+		}
+
+		else{
+			$this->login();
+		}
+
 	}
 
 	public function Privacy(){
+		if($this->session->userdata('client_id'))
 		echo "Privacy";
 	}
 
 	public function Terms(){
+		if($this->session->userdata('client_id'))
 		echo "Terms";		
 	}
 
-	public function addToCart($id, $name, $qty, $price){
-		$data = array(
-			'id' => $id,
-			'qty' => $qty,
-			'price' => $price,
-			'name' => $name
-		);
-		$this->cart->insert($data);
-		echo $this->displayCart();
-		// redirect(base_url('BFF/Order/'));
-	}
-
-	public function getRefCode(){
-		echo '<h5>Your bill is: <strong>'.$this->cart->total().' Pesos</strong></h5><h5>Your reference code will be: '.md5(str_shuffle('0001'.$this->cart->total_items().$this->cart->total())).'</h5>';
-	}
-
-	public function displayCart(){
-		$str = '
-		<thead>
-		  <tr><th>id</th><th>name</th><th>price</th><th>quantity</th><th>total price</th><th>Action</th></tr>
-		</thead>
-		<tbody>';
-		foreach($this->cart->contents() as $items){
-			$str .= '
-		  <tr>        
-		    <td>'.$items['id'].'</td>
-		    <td>'.$items['name'].'</td>
-		    <td>'.$items['price'].'</td>
-		    <td>'.$items['qty'].'</td>
-		    <td>'.$items['price'] * $items['qty'].'</td>
-		    <td>
-		      <a href = "'.base_url('BFF/removeFromCart/'.$items['rowid']).'">Remove from cart</a>
-		    </td>
-		  </tr>';
-		}
-		return $str.'</tbody>';
-	}
-
-	public function removeFromCart($rowid){		
-		$this->cart->remove($rowid);
-		redirect(base_url('BFF/Order/'));
-	}
-
-	public function emptyCart(){
-		$this->cart->destroy();
-		redirect(base_url('BFF/Order/'));
-	}
-
+	public function logout(){
+			$this->session->sess_destroy();
+			redirect(base_url().'home');
+		}		
 }
